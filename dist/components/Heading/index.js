00d8ae19b80f85ae8ebc722eaba03e3d5d86ece7
maxim <totmaxim.r@ya.ru>
2019-09-30 13:54:23 +0400

    new

--- dist/components/Heading/index.js
+++ dist/components/Heading/index.js
@@ -5,10 +5,14 @@ exports["default"] = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+require("./style.css");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var Heading = function Heading() {
-  return _react["default"].createElement("h1", null, "Heading");
+  return _react["default"].createElement("h1", {
+    className: "title"
+  }, "Heading");
 };
 
 var _default = Heading;
--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
 
-const Heading = () => (<h1>Heading</h1>);
+const Heading = () => <h1 className="title">Heading</h1>;
 
 export default Heading;
